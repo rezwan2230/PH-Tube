f3da3ddaf569b1a61b2f7a45fcf63de62268184e
rezwan2230 <rezwan2230@gmail.com>
2023-09-01 03:19:45 +0600

    added card

--- js/phTube.js
+++ js/phTube.js
@@ -22,7 +22,6 @@ const makeHeader = ()=>{
 }
 
 
-
 const dataLoad = async() =>{
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const data = await res.json()
@@ -33,7 +32,6 @@ const dataLoad = async() =>{
 }
 
 const makeAllBtn = (data) =>{
-    console.log(data)
     const allBtnContainer = document.getElementById('all-btn-container')
     data.forEach(element => {
         const div = document.createElement('div')
@@ -45,8 +43,54 @@ const makeAllBtn = (data) =>{
 }
 
 
-const showAllCards = (data) =>{
+const showCategoryItem = async (id) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    const data = await res.json()
+    const actualData = data.data
+    displayCards(actualData)
+}
 
+
+
+const displayCards = (data)=>{
+    console.log(data)
+    const cardContainer = document.getElementById('card-container')
+
+    data.forEach((element)=>{
+        console.log(element)
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card bg-base-100 pb-5">
+                        <figure><img class="w-[500px] h-[320px] " src="${element.thumbnail}" alt="Shoes" /></figure>
+                        <div class="mt-8">
+                            <div class="flex pl-3">
+                                <div>
+                                    <img class="w-16 h-[60px] rounded-full" src="${element.authors[0].profile_picture}" alt="">
+                                </div>
+                                <div class="ml-3 space-y-3">
+                                    <h1 class="text-lg font-bold ">Building a Winning UX Strategy Using the Kano Model
+                                    </h1>
+                                    <div class="flex gap-5">
+                                        <p class="text-base">${element.authors[0].profile_name}</p>
+                                        <p>${element.authors[0].verified}</p>
+                                    </div>
+                                    <p>${element.others.views}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        `
+        cardContainer.appendChild(div)
+    })
+}
+
+
+
+
+const showAllCards = (data) =>{
+    data.forEach((element) =>{
+        showCategoryItem(element.category_id) 
+    })
 }
 
 
